f5ebb590e8ca0428c7eb1daf8c150ce0d7f00c64
Adam Cheasley <adam@netsight.co.uk>
2013-09-16 20:06:41 +0100

    allow the player to move around the world. create a unique key based on the location.

--- tools.py
+++ tools.py
@@ -10,8 +10,25 @@ class Utils(object):
         """)
 
     def parse_user_input(self, user_input, player):
+        room_described = True
+        # remove the verb 'go' as we only care about the direction
+        if user_input.startswith('go'):
+            user_input = user_input[3:]
+
         if user_input == 'help':
             print self.adventure_help()
+        elif user_input == 'look':
+            return False
+        elif user_input in ['east', 'e']:
+            player.current_location[0] += 1
+            room_described = False
+        elif user_input in ['west', 'w']:
+            player.current_location[0] -= 1
+            room_described = False
+        else:
+            print 'I do not understand.\n'
+
+        return room_described
 
 
 class World(object):
@@ -20,9 +37,24 @@ class World(object):
         self.adventure_map = adventure_map
         self.parse_map(self.adventure_map)
 
+    def create_location_id(self, location):
+        return '-'.join(str(x) for x in location)
+
     def parse_map(self, adventure_map):
+        self.world = {}
         for ob in adventure_map:
             room = ob['room']
-            Room(room['title'],
-                 room['description'],
-                 room['location'])
+            location_id = self.create_location_id(room['location'])
+            self.world[location_id] = Room(room['title'],
+                                           room['description'],
+                                           )
+
+    def location(self, current_location):
+        """
+        returns the current room
+        """
+        location = self.world.get(self.create_location_id(current_location))
+        if location is not None:
+            return location.long_description
+        else:
+            return 'You are lost.\n'
