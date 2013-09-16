eb531a725db7eaac0894fd7f74050797d4264698
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2013-09-16 15:53:36 +0100

    a basis for the major objects needed

---
+++ content.py
@@ -0,0 +1,34 @@
+class player(object):
+
+    def take(self):
+        pass
+
+    def drop(self):
+        pass
+
+    def inventory(self):
+        pass
+
+    def location(self):
+        pass
+
+
+class item(object):
+
+    def description(self):
+        pass
+
+
+class room(object):
+
+    def long_description(self):
+        pass
+
+    def short_description(self):
+        pass
+
+    def location(self):
+        pass
+
+    def items(self):
+        pass
