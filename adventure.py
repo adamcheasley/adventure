266be882c78894372ee54a7b2223158ddaca132f
Adam Cheasley <adam@netsight.co.uk>
2013-09-16 20:08:00 +0100

    the adventure now knows whether to display the room's description or not

--- adventure.py
+++ adventure.py
@@ -12,12 +12,16 @@ print "Welcome player.\n"
 # initialise the map
 world = World(world_info)
 utils = Utils()
-player = Player((0, 0, 0))
+player = Player([0, 0, 0])
+room_described = False
 
 while True:
+    if not room_described:
+        print '%s\n' % world.location(player.current_location)
+
     user_input = raw_input('>:')
     if user_input == 'exit':
         print 'Goodbye\n'
         sys.exit(1)
     else:
-        utils.parse_user_input(user_input, player)
+        room_described = utils.parse_user_input(user_input, player)
