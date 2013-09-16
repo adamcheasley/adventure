7d6b2f15eacf82e011e9e4670ce5bddfc17d442e
Adam Cheasley <adam@netsight.co.uk>
2013-09-16 20:06:58 +0100

    add another test room

--- world.py
+++ world.py
@@ -2,8 +2,13 @@
 
 world = [
     {'room': {'title': 'lobby',
-              'description': ('foo', 'bar'),
-              'location': (0, 0, 0)},
+              'description': ('You are in the lobby. To the east is a door.', 'bar'),
+              'location': [0, 0, 0]},
      'items': [],
+     },
+    {'room': {'title': 'room 2',
+              'description': ('foo', 'bar'),
+              'location': [1, 0, 0]},
+     'items': [{'title': 'key'}],
      },
 ]
